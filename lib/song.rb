a081cfde417635282b91ee0ff2cb3733f89ebdc7
Maryna Voitenko <maryna.voitenko@gmail.com>
2017-11-08 03:57:42 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,4 +34,7 @@ class Song
     self.all.detect {|song| song.name == name}
   end
 
+  def self.find_or_create_by_name(name)
+    self.create_by_name(name) if !self.all.find_by_name(name)
+  end
 end
