89d7c715a292ab01918b12dd56104cd427435a2b
Maryna Voitenko <maryna.voitenko@gmail.com>
2017-11-08 03:59:49 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,11 +35,12 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    song = self.find_by_name(name)
-    if song
-      return song
-    else
-      self.create_by_name(name)
-    end
+    self.create_by_name(name) if !self.find_by_name(name)
+    # song = self.find_by_name(name)
+    # if song
+    #   return song
+    # else
+    #   self.create_by_name(name)
+    # end
   end
 end
