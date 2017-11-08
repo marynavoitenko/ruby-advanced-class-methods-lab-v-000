e967c0e8ee1123c08ff40fe0f69e936279fc9d1f
Maryna Voitenko <maryna.voitenko@gmail.com>
2017-11-08 04:39:50 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -48,7 +48,7 @@ class Song
   end
 
   def self.new_from_filename(filename)
-    song = self.new
+    song = self.create
     filename_array = filename.split(/\.|-/)
     song.artist_name = filename_array[0].strip!
     song.name = filename_array[1].strip!
